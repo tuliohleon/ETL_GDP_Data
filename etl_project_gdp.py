7255502a6df3696d6312250e37b7681d9f9e8dcb
tuliohleon <tuliohleon@gmail.com>
2024-12-30 23:27:28 -0500

    Refactor ETL process to improve GDP data extraction and transformation; add logging and save results to CSV and database.

--- etl_project_gdp.py
+++ etl_project_gdp.py
@@ -39,12 +39,12 @@ def extract(url, table_attribs):
                 
                 # Extract data for each attribute
             country = cells[0].find('a').text.strip()
-            gdp_usd_millions = cells[2].text.strip().replace(',', '').replace('$', '')
+            gdp_usd_millions = cells[2].text.strip()
 
                 # Store valid entries in a dictionary
             entry = {
                 table_attribs[0]: country,
-                table_attribs[1]: float(gdp_usd_millions) if gdp_usd_millions else None
+                table_attribs[1]: gdp_usd_millions if gdp_usd_millions else None
                 }
 
                 # Add the entry to the list of rows to add
@@ -60,19 +60,27 @@ def transform(df):
     USD (Millions) to USD (Billions) rounding to 2 decimal places.
     The function returns the transformed dataframe.'''
 
+    df['GDP_USD_millions'] = df['GDP_USD_millions'].str.replace(',', '').str.replace('$', '').astype(float)
+    df['GDP_USD_millions'] = (df['GDP_USD_millions']/1000).round(2)
+    df.rename(columns={'GDP_USD_millions': 'GDP_USD_billions'}, inplace=True)   
+
     return df
 
 def load_to_csv(df, csv_path):
     ''' This function saves the final dataframe as a `CSV` file 
     in the provided path. Function returns nothing.'''
+    load_to_csv(df, csv_path)
 
 def load_to_db(df, sql_connection, table_name):
     ''' This function saves the final dataframe as a database table
     with the provided name. Function returns nothing.'''
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
 
 def run_query(query_statement, sql_connection):
     ''' This function runs the stated query on the database table and
     prints the output on the terminal. Function returns nothing. '''
+    query_result = pd.read_sql(query_statement, sql_connection)
+    print(query_result)
 
 def log_progress(message):
     ''' This function logs the mentioned message at a given stage of the code execution to a log file. Function returns nothing'''
@@ -82,5 +90,18 @@ table_attribs = ["Country", "GDP_USD_millions"]
 db_name = 'World_Economies.db'
 table_name = 'Countries_by_GDP'
 csv_path = './Countries_by_GDP.csv'
-
+log_progress('Preliminaries complete. Initiating ETL process.')
 df=extract(url, table_attribs)
+log_progress('Call extract() function	Data extraction complete. Initiating Transformation process.')
+df=transform(df)
+log_progress('Data transformation complete. Initiating loading process.')
+load_to_csv(df, csv_path)
+log_progress('Data saved to CSV file.')
+sql_connection = sqlite3.connect(db_name)
+log_progress('SQL Connection initiated.')
+load_to_db(df, sql_connection, table_name)
+log_progress('Data loaded to Database as table. Running the query.')
+query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+run_query(query_statement, sql_connection)
+log_progress('	Process Complete.')
+sql_connection.close()
